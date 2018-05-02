8d0774dfac1a2b14c9741783e4bd459fa62d7ab5
unknown <cbejar93@gmail.com>
2018-05-02 08:22:49 -0400

    saved

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,18 @@
 var amigosData = [
     {
         name: "Carlos",
+        score: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            5,
+            5,
+            4,
+            
+        ]
 
     }
 ]
